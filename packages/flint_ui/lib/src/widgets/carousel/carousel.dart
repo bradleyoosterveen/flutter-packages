1df3457bffc712e039c12b3e1773b706e6939aa7
Bradley Oosterveen <bradleyoosterveen@gmail.com>
2026-09-19 20:08:53 +0200

    Attempt to improve scaffold and carousel

--- packages/flint_ui/lib/src/widgets/carousel/carousel.dart
+++ packages/flint_ui/lib/src/widgets/carousel/carousel.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flint_ui/flint_ui.dart';
 
 class FlintUiCarousel extends StatefulWidget {
   const FlintUiCarousel({required this.items, super.key});
@@ -16,7 +15,7 @@ class _FlintUiCarouselState extends State<FlintUiCarousel> {
   @override
   void initState() {
     super.initState();
-    _pageController = PageController(viewportFraction: 0.9);
+    _pageController = PageController(viewportFraction: 1);
   }
 
   @override
@@ -31,46 +30,35 @@ class _FlintUiCarouselState extends State<FlintUiCarousel> {
   }
 
   @override
-  Widget build(BuildContext context) => AspectRatio(
-    aspectRatio: 1,
-    child: FlintUiFlex.column(
-      divider: FlintUiGap.column(context.themeData.spacing.md),
-      children: [
-        Expanded(
-          child: PageView.custom(
-            pageSnapping: false,
-            physics: const PageScrollPhysics(),
-            clipBehavior: Clip.none,
-            controller: _pageController,
-            childrenDelegate: SliverChildBuilderDelegate((context, index) {
-              final child = widget.items[index];
+  Widget build(BuildContext context) => PageView.custom(
+    pageSnapping: false,
+    physics: const PageScrollPhysics(),
+    controller: _pageController,
+    childrenDelegate: SliverChildBuilderDelegate((context, index) {
+      final child = widget.items[index];
 
-              return AnimatedBuilder(
-                animation: _pageController,
-                builder: (context, _) {
-                  final page = _pageController.page ?? 0;
-                  final offset = page - index;
-                  final scaleValue = Curves.easeOut.transform(
-                    (1 - (offset.abs() * .5)).clamp(0.0, 1.0),
-                  );
+      return AnimatedBuilder(
+        animation: _pageController,
+        builder: (context, _) {
+          final page = _pageController.page ?? 0;
+          final offset = page - index;
+          final scaleValue = Curves.easeOut.transform(
+            (1 - (offset.abs() * .5)).clamp(0.0, 1.0),
+          );
 
-                  final alignment = offset < 0
-                      ? Alignment.centerLeft
-                      : offset > 0
-                      ? Alignment.centerRight
-                      : Alignment.center;
+          final alignment = offset < 0
+              ? Alignment.centerLeft
+              : offset > 0
+              ? Alignment.centerRight
+              : Alignment.center;
 
-                  return Transform.scale(
-                    scale: scaleValue,
-                    alignment: alignment,
-                    child: child,
-                  );
-                },
-              );
-            }, childCount: widget.items.length),
-          ),
-        ),
-      ],
-    ),
+          return Transform.scale(
+            scale: scaleValue,
+            alignment: alignment,
+            child: child,
+          );
+        },
+      );
+    }, childCount: widget.items.length),
   );
 }
